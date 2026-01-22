8ac85f86f5f05741d9d7d600334ff393fbbd6d28
Hemiharper <jim.townend@gmail.com>
2026-01-22 22:55:20 +1100

    Create cocktail_db.ts with cocktail recipes
    Added a full cocktail database with various recipes.

---
+++ cocktail_db.ts
@@ -0,0 +1,138 @@
+// cocktail_db.ts
+export const FULL_COCKTAIL_DATABASE = `
+1. Boston: 45ml Gin, 45ml Apricot Brandy, 25ml Lemon Juice, 10ml Grenadine
+2. Aviation: 45ml Gin, 25ml Creme De Violette, 25ml Lemon Juice, 15ml Maraschino Liqueur
+3. Bloody Floradora: 60ml Bloody Shiraz, 25ml Lime Juice, 60ml Ginger Beer
+4. Bramble: 45ml Gin, 15ml Marionette Mure, 20ml Lemon Juice, 10ml Sugar Syrup
+5. White Lady: 30ml Gin, 20ml Orange Curaçao, 20ml Lemon Juice, 5ml Sugar Syrup, 15ml Egg White
+6. Cucumber Gimlet: 45ml Gin, 30ml Lime Juice, 15ml Sugar Syrup, Muddled Cucumber & Mint
+7. Limoncello Gin: 30ml Gin, 15ml Limoncello, 15ml Lime, 10ml Sugar Syrup
+8. Negroni: 30ml Gin, 30ml Vermouth Red, 30ml Campari
+9. Who Shot Tom Collins: 30ml Gin, 20ml Lemon Juice, 10ml Sugar Syrup, 90ml Soda, 15ml Bloody Shiraz Float
+10. Gin Mule: 30ml Navy Strength Gin, 15ml Lime Juice, 2 Dashes Bitters, Capi Ginger Beer
+11. Five & Dime: 45ml Gin, 15ml Apricot Brandy, 25ml Lemon Juice, 15ml Orgeat, 2 Dashes Bitters
+12. Saturn (Standard): 45ml Gin, 25ml Lemon Juice, 15ml Chinola, 15ml Orgeat, 10ml Falernum
+13. Sunflower: 25ml Gin, 20ml Triple Sec, 20ml Elderflower, 25ml Lemon Juice, 2 Dashes Absinthe
+14. Silk Road: 45ml Gin, 10ml Dom Benedictine, 10ml Apricot Brandy, 15ml Lime Juice, 10ml Sugar Syrup, Dash Bitters
+15. Margarita: 45ml Tequila, 25ml Orange Curacao, 25ml Lime Juice, 10ml Agave Syrup, Pinch Salt (Optional Habanero)
+16. The Heartland Smash: 45ml Reposado Tequila, 15ml Aperol, 10ml Agave Nectar, 30ml Lemon Juice, Bitters, Pineapple & Mint
+17. Paloma: 30ml Tequila, 15ml Lime Juice, 30ml GF Juice, Splash Soda
+18. Pink GF Margarita: 30ml Tequila/Mezcal, 10ml Agave Syrup, 20ml GF Cordial, 25ml Lime Juice (Optional Habanero)
+19. Tick The Clock: 45ml Blanco Tequila, 30ml Lime Juice, 20ml Ginger Syrup, 5ml Angostura Bitters
+20. Part-time Lover: 45ml Blanco Tequila, 15ml Aperol, 15ml Elderflower, 25ml Lemon Juice, 2 Dash Bitters
+21. Corpse Reviver #4: 30ml Reposado Tequila, 20ml Lillet Blanc, 10ml Orange Curacao, 20ml Lemon Juice, 5ml Sugar Syrup, 1 Dash Absinthe
+22. Mexican Firing Squad: 60ml Reposado Tequila, 25ml Lime Juice, 15ml Grenadine, 2 Dash Bitters, Splash Soda
+23. Tommy’s Margarita: 60ml Tequila, 30ml Lime Juice, 15ml Agave Syrup (Optional Habanero)
+24. Jungle Booby: 45ml Blanco Tequila, 15ml Mezcal, 15ml Campari, 15ml Orgeat, 15ml Lime, 15ml GF Juice, 30ml Pineapple, 4 Dash Absinthe
+25. Cannonball: 60ml Mezcal, 10ml Apricot Brandy, 25ml Pineapple Juice, 15ml Lime Juice, Dash Bitters
+26. Smoke & Mirrors: 45ml Mezcal, 15ml Orange Curacao, 30ml Lime Juice, 10ml Ginger Syrup, 10ml Pineapple Syrup, 1 Egg White, 1 Drop Jalapeno
+27. Singapore Sling: 45ml Gin, 15ml Maraschino, 10ml Dom Benedictine, 10ml Orange Curacao, 45ml Pineapple, 15ml Lime, 5ml Grenadine, 1 Dash Bitters
+28. Lizey’s Conquistador: 30ml Blanco Tequila, 30ml Gold Rum, 15ml Simple Syrup, 15ml Lemon Juice, 15ml Lime, 2 Dash Orange Bitters, Egg White
+29. Deadman's Handle: 45ml Blanco Tequila, 15ml Aperol, 15ml Lime Juice, 15ml Orgeat (Optional Habanero)
+30. Smokin’ Berries: 45ml Mezcal, 15ml Drambuie, 15ml Mure, 30ml Lime Juice, 2 Dashes Peychaud's
+31. Avion De Papal: 25ml Blanco Tequila, 25ml Amaro, 25ml Aperol, 25ml Lemon Juice
+32. Clearin’ Smoke: 45ml Mezcal, 15ml Aperol, 10ml Ginger Cordial, 25ml Lime, 25ml Pineapple
+33. Corpse Reviver #2: 30ml Gin, 30ml Orange Curacao, 30ml Lillet Blanc, 30ml Lemon Juice, 5ml Syrup, 2 Dash Absinthe
+34. Water Lily: 25ml Gin, 25ml Grand Mariner, 25ml Crème De Violette, 25ml Lemon Juice
+35. Spicy Dead Lady: 25ml Mezcal, 25ml Aperol, 25ml Falernum, 25ml Lime Juice, 1 Dash Jalapeno
+36. Old Friend: 45ml Gin, 25ml GF Juice, 10ml Campari, 15ml Elderflower
+37. Hey Mambo: 25ml Aperol, 25ml Mezcal, 25ml Tequila, 45ml Pineapple, 45ml Coconut Cream, 15ml Lime
+38. Last Word: 25ml Mezcal/Gin, 25ml Lime Juice, 25ml Maraschino Liqueur, 15ml Green Chartreuse
+39. Court Royale: 30ml London Dry Gin, 25ml Lillet Blanc, 25ml Lemon Juice, 15ml Applejack, 15ml Simple Syrup
+40. Biggles Aviation: 45ml Gin, 15ml Ginger Liqueur, 15ml Lemon Juice, 10ml Sugar Syrup, 5ml Crème De Violette, 2 Dash Peychaud's
+41. Pink Lady: 45ml Gin, 25ml Lemon Juice, 15ml Applejack, 15ml Grenadine, 10ml Sugar Syrup, Egg White
+42. Oaxaca Old Fashioned: 45ml Reposado Tequila, 15ml Mezcal, 10ml Agave Syrup, 2 Dash Bitters
+43. Ginger Marg: 60ml Reposado Tequila, 30ml Ginger Liqueur, 30ml Lime Juice, 10ml Agave Syrup
+44. Negroni and The Goat: 45ml Gin, 25ml Aperol, 25ml Vermouth, 15ml Amaro
+45. Whisky Melon: 30ml Whisky, 60ml Watermelon, 10ml Sugar Syrup, Splash Soda, Muddled Cucumber
+46. Whisky Sour: 30ml Whisky, 15ml Lemon Juice, 10ml Sugar Syrup, Bitters (Optional Egg White)
+47. Apricot Sour: 40ml Bourbon, 20ml Apricot Brandy, 25ml Lemon Juice, 10ml Sugar Syrup, Egg White, Dash Bitters
+48. Amaretto Sour: 45ml Amaretto, 30ml Bourbon, 30ml Lemon Juice, 5ml Simple Syrup, Egg White
+49. Paper Plane: 25ml Bourbon, 25ml Aperol, 25ml Amaro, 25ml Lemon Juice
+50. Pisco Sour: 60ml Pisco, 25ml Lime Juice, 25ml Sugar Syrup, Egg White
+51. Brandy Crusta: 45ml Cognac, 15ml Orange Curacao, 15ml Lemon Juice, 15ml Maraschino Liqueur, 2 Dashes Bitters
+52. Duo Chin: 60ml Pisco, 15ml Apricot Brandy, 15ml Grand Mariner, 15ml Lime Juice, 10ml Grenadine
+53. Playmate Martini: 25ml Cognac, 25ml Orange Curacao, 25ml Apricot Brandy, 25ml OJ, Egg White
+54. Penicillin: 60ml Whisky, 25ml Ginger Liqueur, 25ml Lemon Juice, 15ml Honey Syrup, 10ml Laphroaig Float
+55. Irish Maid: 60ml Bourbon, 15ml Elderflower, 25ml Lemon Juice, 10ml Sugar Syrup, Muddled Cucumber/Mint
+56. Millionaire: 60ml Bourbon, 25ml Orange Curacao, 15ml Lemon Juice, 15ml Grenadine, 5ml Absinthe, Egg White
+57. Ode To Viceroy: 45ml Scotch, 25ml Lime Juice, 15ml Falernum
+58. Surf Liner: 60ml Rye Whiskey, 30ml Pineapple Juice, 25ml Lemon Juice, 15ml Orgeat, 2 Dashes Peychaud’s
+59. Biggles Sidecar: 45ml Cognac, 25ml Lemon Juice, 15ml Ginger Liqueur, 10ml Sugar Syrup, 10ml Crème De Violette, 2 Dash Peychaud's
+60. Iron Ranger: 60ml Bourbon, 25ml Lemon Juice, 30ml Pineapple Juice, 15ml Falernum, 2 Dash Bitters
+61. Whisky Daisy: 30ml Bourbon, 30ml Rye Whisky, 30ml Lemon Juice, 20ml Orgeat
+62. Polynesian Paralysis: 60ml Bourbon, 30ml Orange Juice, 30ml Pineapple Juice, 25ml Lemon Juice, 15ml Orgeat
+63. Little Hugo: 20ml Elderflower, 60ml Lillet Blanc, Soda Water, Cucumber & Mint (Optional 15ml Gin)
+64. Aperol Spritz: 3 Parts Dry Prosecco, 2 Parts Aperol, 1-2 Parts Soda
+65. Rhubarb Spritz: 45ml Rhubi Liqueur, 30ml Vermouth, 15ml Gin, Soda
+66. 12 Mile: 30ml Gold Rum, 15ml Rye Whisky, 15ml Cognac, 10ml Grenadine, 15ml Lemon Juice
+67. Mojito: 60ml Light Rum, 15ml Sugar Syrup, 60ml Soda, 8 Mint Leaves, 1/2 Lime (Muddled)
+68. Daiquiri: 60ml Rum, 30ml Lime, 15ml Sugar Syrup
+69. Pina Colada: 45ml Rum, 45ml Pineapple Juice, 30ml Coco Lopez (Blended)
+70. Caipirinha: 60ml Cachaca, 3/4 Lime Wedges, 1.5 Tsp Sugar (Muddled)
+71. Mai Tai: 30ml Appleton 12, 30ml Clement VSOP, 25ml Lime, 15ml Orange Curacao, 15ml Orgeat, 2 Dash Bitters
+72. Hotel Nacional: 25ml Light Rum, 25ml Pineapple Rum, 15ml Apricot Liqueur, 25ml Pineapple Juice, 15ml Lime, 5ml Sugar Syrup, 1 Dash Bitters
+73. Scorpion Reef: 30ml Mezcal, 30ml Jamaican Rum, 30ml Pineapple, 25ml Lemon, 25ml Orgeat, 5ml Crème De Cacao
+74. Dirty Dicks: 52.5ml Dark Rum, 15ml Orgeat, 10ml Ginger Syrup, 10ml Honey Syrup, 30ml Lemon, 5ml Whisky
+75. Train Wreck: 45ml Gold Rum, 45ml Pineapple Juice, 25ml Ginger Syrup, 30ml Lemon Juice, 1 Dash Bitters, 1 Dash Peychaud's
+76. Cane Flower: 45ml Cachaca, 25ml Aperol, 15ml Elderflower
+77. Tai Du Mai: 30ml Cognac, 30ml Lime, 15ml Gold Rum, 15ml Pineapple, 15ml Orgeat, 10ml Orange Curacao
+78. Zombie: 45ml Gold Rum, 30ml OP Rum, 15ml Falernum, 25ml Pineapple, 25ml Lime, 15ml GF Juice, 10ml Grenadine, 10ml Cinnamon Syrup, 2 Dash Absinthe
+79. Navy Grog: 30ml Dark Rum, 30ml Demerara Rum, 30ml Light Rum, 30ml Pineapple, 15ml GF Cordial, 25ml Lime, 15ml Falernum, 3 Dash Bitters
+80. Chief Lapu Lapu: 30ml Dark Rum, 30ml Light Rum, 60ml OJ, 30ml Lemon, 30ml Chinola, 25ml Falernum, 10ml Vanilla Syrup
+81. Cobras Fang: 45ml OFTD, 25ml Lime, 25ml Orange, 15ml Chinola/Falernum, 10ml Grenadine, 1 Dash Bitters, 1 Dash Absinthe
+82. Rum Barrel: 30ml Dark Rum, 30ml Gold Rum, 25ml Lime, 25ml Orange, 25ml Pineapple, 25ml Chinola, 15ml Simple Syrup
+83. Goonies Never Say Die: 45ml Gold Rum, 30ml Light Rum, 30ml Pineapple, 25ml Lemon, 15ml Amaretto, 15ml Orgeat, 10ml Falernum, Bitters
+84. Zombie Riff: 25ml Reposado Tequila, 25ml Gold Rum, 25ml Lime, 15ml Mezcal, 15ml Falernum, 15ml GF Juice, 10ml Grenadine, 10ml Cinnamon Syrup
+85. Test Pilot: 30ml Jamaican Rum, 15ml OP Rum, 25ml Light Rum, 25ml Falernum, 15ml Orange Curacao, 15ml Lime, 1 Dash Bitters, 3 Dash Absinthe
+86. Green Eyes: 45ml Gin, 25ml Lime, 15ml Green Chartreuse, 15ml Sugar Syrup, Egg White
+87. Brass Rail: 45ml Dark Rum, 10ml Dom Benedictine, 15ml Lemon, 10ml Sugar Syrup, Egg White, Dash Orange Bitters
+88. Chartreuse Swizzle: 45ml Green Chartreuse, 30ml Pineapple, 25ml Lime, 15ml Falernum
+89. Brasil: 40ml Cachaca, 20ml Yellow Chartreuse, 25ml Lemon, 10ml Sugar Syrup
+90. Jet Pilot: 30ml Black Rum, 25ml Gold Rum, 25ml OFTD, 15ml Falernum, 15ml Cinnamon Syrup, 15ml GF Juice, 15ml Lime, 1 Dash Bitters, 1 Dash Absinthe
+91. Bali Bali: 15ml Gold Rum, 15ml Dry Gin, 15ml Cognac, 15ml Light Rum, 15ml Falernum, 10ml Chinola, 15ml Lime, 15ml Orange, 15ml Pineapple
+92. Baron Samedi: 52.5ml Martinique, 25ml Lime, 10ml Cinnamon Syrup, 10ml Falernum
+93. Don’s QB Cooler: 30ml Jamaican Rum, 15ml Demerara, 30ml Light Rum, 30ml Lime, 30ml Orange, 15ml Chinola, 15ml Honey Syrup, 15ml Falernum, 2 Dash Bitters
+94. Painkiller: 60ml Dark Rum, 45ml Pineapple, 30ml Orange, 30ml Coconut Cream, Nutmeg
+95. Beachbum: 30ml Light Rum, 30ml Gold Rum, 30ml Pineapple, 25ml Lime, 15ml Apricot Brandy, 15ml Orgeat
+96. Scorpion: 30ml Bacardi 8, 30ml Gin, 15ml Cognac, 30ml Orange, 15ml Orgeat, 10ml Sugar Syrup, 15ml Lime (Blended)
+97. 3 Dots and a Dash: 45ml Agricole, 15ml Demerara Rum, 10ml Falernum, 10ml Allspice Dram, 15ml Lime, 15ml Orange, 15ml Honey Syrup, Dash Bitters
+98. Dr. Funk: 45ml Agricole, 15ml Lemon, 15ml Grenadine, 10ml Absinthe
+99. Tonga Thunder: 30ml OFTD, 30ml Agricole, 25ml GF Juice, 25ml Orange, 25ml Lemon, 15ml Cinnamon Syrup, 10ml Allspice Dram, Peychaud's Float
+100. Juke Cup: 45ml Rum Agricole, 15ml Falernum, 25ml Lime, 10ml Honey Syrup, Ginger Beer, Muddled Cucumber & Pineapple
+101. Missionary’s Downfall: 45ml Light Rum, 15ml OFTD, 15ml Peach Liqueur, 15ml Falernum, 15ml Honey Syrup, 25ml Lime, Muddled Mint & Pineapple, Black Rum Float
+102. Blackbeards Ghost: 30ml Light Rum, 15ml OFTD, 15ml Demerara Rum, 15ml Apricot Brandy, 30ml Lemon, 45ml Orange, 15ml Falernum
+103. Bermuda Swizzle: 30ml Black Rum, 30ml Gold Rum, 25ml Orange Curacao, 25ml Lemon, 50ml Pineapple, 50ml Orange, 15ml Falernum, 2 Dash Bitters
+104. For Pete’s Sake: 45ml Pisco, 25ml Lime, 15ml GF Juice, 15ml Maraschino, 15ml Hibiscus Liqueur, 1 Dash Bitters
+105. Piñata: 30ml Black Rum, 30ml Light Rum, 90ml Pineapple, 30ml Lemon, 10ml Demerara Syrup, 30ml Ginger Liqueur, 15ml Allspice Dram
+106. Captains Grog: 25ml Black Rum, 25ml Light Rum, 25ml Gold Rum, 15ml Lime, 15ml GF Juice, 15ml Maple Syrup, 3 drops Vanilla/Almond, 15ml Falernum, 15ml Orange Curacao
+107. Peachtree Punch 2.0: 60ml Light Rum, 90ml Orange Juice, 15ml Lemon, 30ml Coco Lopez, 15ml Peach Liqueur, Muddled 1/2 Peach
+108. Roselita: 45ml Bacardi 8, 15ml Pear Liqueur, 15ml Hibiscus Liqueur, 15ml Orgeat, 25ml Lime, 1 Dash Peychaud's
+109. Finkelgrog: 30ml Black Rum, 30ml Light Rum, 15ml Licor 43, 15ml Lillet, 15ml GF Juice, 15ml Lime, 10ml Cinnamon Syrup
+110. SC Rum Barrel: 60ml El Dorado, 15ml OFTD, 30ml Lime, 30ml Pineapple, Dash Bitters, 10ml Vanilla Syrup, 10ml Dram, 10ml Molasses
+111. La Guildive: 60ml Gold Rum, 10ml Peach Liqueur, 15ml Licor 43, 15ml Lime, Ginger Beer
+112. Pornstar Martini: 30ml Vanilla Vodka, 25ml Chinola, 25ml Lime Juice, 10ml Vanilla Syrup
+113. Mudslide: 30ml Vodka, 30ml Baileys, 30ml Coffee Liqueur, 60-90ml Milk, 10ml Crème De Cacao
+114. Espresso Martini: 30ml Vodka/Rum, 30ml Coffee Liqueur, 30ml Espresso, 10ml Crème De Cacao
+115. Sweet Dream: 15ml Light Rum, 15ml Crème de Cacao, 10ml Crème de Banana, 15ml Pineapple, 15ml Cream, Nutmeg
+116. Ti’ Punch: 60ml Blanc Agricole, 15ml Sugarcane Syrup, Lime
+117. The Naked Ape: 45ml Dark Rum, 15ml OFTD, 15ml Lemon, 15ml Cinnamon Syrup, 15ml Banana Liqueur, 1 Dash Bitters
+118. Aku Aku: 45ml Light Rum, 15ml Peach Liqueur, 30ml Lime, 15ml Demerara Syrup, Muddled Mint & Pineapple
+119. The Nui Nui: 60ml Latin Rum, 7.5ml Allspice Dram, 25ml Orange, 25ml Lime, 7.5ml Vanilla Syrup, 7.5ml Cinnamon Syrup, Dash Bitters
+120. Don’s Own Grog: 30ml Gold Rum, 15ml Light Rum, 15ml Black Rum, 15ml Mure, 25ml Lime, 7.5ml Sugar Syrup, Dash Grenadine/Bitters, Nutmeg
+121. Port au Prince: 45ml Barbancourt Rum, 15ml Falernum, 15ml Lime, 15ml Pineapple, 7.5ml Sugar Syrup, Dash Grenadine/Bitters
+122. Golden Gun: 30ml Light Rum, 30ml Gold Rum, 15ml Apricot Brandy, 25ml Lime, 15ml GF Juice, 15ml Sugar Syrup, 2 Dash Bitters
+123. Merciless Virgin: 45ml Light Rum, 15ml Falernum, 15ml Maraschino, 10ml Orange Curacao, 25ml Lemon
+124. Scofflaw: 45ml Rye Whiskey, 30ml Dry Vermouth, 25ml Lime Juice, 15ml Grenadine, 1 Dash Orange Bitters
+125. Jasmine Cocktail: 45ml Gin, 25ml Lemon Juice, 10ml Campari, 10ml Orange Curacao
+126. Dead Rabbit: 45ml Whisky, 30ml Sweet Vermouth, 15ml Green Chartreuse, 10ml Simple Syrup, 2 Dashes Orange Bitters
+127. Agave Flower: 30ml Mezcal, 25ml Aperol, 15ml Maraschino Liqueur, 25ml Lime Juice
+128. Periscope: 45ml Gin, 30ml Elderflower, 10ml Lemon Juice, Egg White
+129. Southside: 60ml Gin, 25ml Lemon Juice, 15ml Sugar Syrup
+130. Bees Knees: 60ml Gin, 25ml Lemon Juice, 25ml Honey Syrup
+131. Pegu Club: 60ml Gin, 25ml Lime Juice, 25ml Curacao, 1 Dash Bitters, 1 Dash Orange Bitters
+132. Saturn #2: 30ml Gin, 15ml Gold Rum, 15ml Campari, 15ml Sweet Vermouth, 15ml Falernum, 30ml Lemon, 15ml Chinola, 15ml Orgeat
+133. Manhattan: 60ml Rye Whiskey, 30ml Sweet Vermouth, 2 Dash Bitters
+134. Old Fashioned: 60ml Whisky/Bourbon, 10ml Sugar Syrup, 3 Dash Bitters
+135. Hibiscus Paloma: 30ml Blanco Tequila, 22.5ml Hibiscus Syrup, 15ml Lime, 15ml GF Cordial, Soda Water
+`;
